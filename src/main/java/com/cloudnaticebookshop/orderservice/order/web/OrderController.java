6810ab7f359a132b17009f8e9e524e6089a18ee7
Shant Dashjian <shantgdashjian@gmail.com>
2024-03-15 15:10:18 +0400

    Add dev tools and endpoint

--- src/main/java/com/cloudnaticebookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/cloudnaticebookshop/orderservice/order/web/OrderController.java
@@ -3,14 +3,12 @@ package com.cloudnaticebookshop.orderservice.order.web;
 import com.cloudnaticebookshop.orderservice.order.domain.Order;
 import com.cloudnaticebookshop.orderservice.order.domain.OrderService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("orders")
 public class OrderController {
 	private final OrderService orderService;
 
